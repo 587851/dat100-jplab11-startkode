4c1a41b1eae65aefefba3a904e34a2ca5e1db1dd
torje <torje@192.168.10.138>
2020-11-04 12:33:44 +0100

    Ferdig med oblig oppgaver

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -9,7 +9,15 @@ import no.hvl.dat100.jplab11.oppgave3.*;
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
+ 
+		
+		try {		
+		PrintWriter skriver = new PrintWriter(mappe + filnavn);
+		skriver.print(samling.toString());
+		skriver.close();
+		return true;
+		}catch(FileNotFoundException e) {System.out.println("File not found!");
+	    }
+		return false;
 	}
 }
